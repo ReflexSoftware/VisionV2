dd4dfb4b7c7aad2ee7bdb23290c4647c3c6496b8
Reflex <reflexsoftware@coolsite.net>
2024-05-15 21:59:01 -0400

    started working on this

---
+++ src/main/java/com/chloecodesthings/visionv2/VisionV2.java
@@ -0,0 +1,17 @@
+package com.chloecodesthings.visionv2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VisionV2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
